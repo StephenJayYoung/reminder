9aac54623fa60de2b0830072dbcf9af32a034523
Stephen Young <StephenJayYoung@gmail.com>
2014-09-09 13:31:16 -0700

    Starting reminder function.

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Execute a callback at a given time.
+ *
+ * @param {Date} date - Date at which we want the callback to be called.
+ * @param {Function} cb - The callback function.
+ */
+module.exports.setReminder = function() {
+	// TODO: function goes here
+
+};
---
+++ test/test.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var expect = chai.expect;
+var functions = require('../index');
+var setReminder = functions.setReminder;
+
+
+describe('reminds us to take lunch', function() {
+
+  it('should tell us to go to lunch', function(done) {
+  	// TODO: send real stuff to this function
+  	setReminder();
+  	// var timer('should go off at a certain time')
+  });
+});
